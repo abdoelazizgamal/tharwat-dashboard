c11e9e1b92cfee630afa4dbd50cab4b96ca7f161
Mohamed Fathy <121361427+moofat7y@users.noreply.github.com>
2025-05-04 15:28:58 +0300

    Update authApi.js

--- src/store/api/authApi.js
+++ src/store/api/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({ 
-    baseUrl: 'https://reportsqrcode.onrender.com/api/v1',
+    baseUrl: 'https://api.g-so.com/api/v1',
   }),
   endpoints: (builder) => ({
     signin: builder.mutation({
